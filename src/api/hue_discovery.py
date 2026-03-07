e7b6f1833335a23437a29a0242ea079ebff93053
Reketino <bjornevensk8@gmail.com>
2026-03-07 10:12:11 +0100

    Feat: setup for python automatically discovering ip address

--- src/api/hue_discovery.py
+++ src/api/hue_discovery.py
@@ -1,4 +1,14 @@
 import requests
 
-def 
+def  discover_bridge_ip() -> str | None:
+    try:
+        data = requests.get("https://discovery.meethue.com", timeout=5).json()
+        
+        if data: 
+            return data[0]["internalipaddress"]
+        
+    except Exception as e:
+        print("Could not find Bear's Hue Bridge", e)
+        
+        return None
 
