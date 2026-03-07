b4a8dfc710b3fe58a9d40afac76f3bc9c7ce580e
Reketino <bjornevensk8@gmail.com>
2026-03-08 00:13:57 +0100

    Feat: logic behind main script set up

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -2,4 +2,11 @@ from api.hue_discovery import discover_bridge_ip
 from api.hue_api import HueAPI
 
 def main():
+    bridge_ip = discover_bridge_ip()
+    
+    hue = HueAPI(bridge_ip)
+    
+    lights = hue.get_lights()
+    
+    print(lights)
     
