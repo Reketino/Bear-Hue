8b0e04eb93259bb45e191d70608266390a5665dd
Reketino <bjornevensk8@gmail.com>
2026-03-07 16:18:28 +0100

    Feat: test logic of API setup

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,24 @@
 from api.hue_discovery import discover_bridge_ip
 from api.hue_api import HueAPI
+
+def run_test():
+    
+    bridge_ip = discover_bridge_ip()
+    
+    if not bridge_ip:
+        print("We can't seem to find ur Hue Bridge on the network.")
+        return
+    
+    hue = HueAPI(bridge_ip)
+    
+    lights = hue.get_lights()
+    
+    print("\nWe Found your lights:\n")
+    
+    for light_id, light in lights.items():
+        print(f"{light_id}: {light['name']}")
+        
+if __name__ == "__main__":
+    run_test()
+    
+    
