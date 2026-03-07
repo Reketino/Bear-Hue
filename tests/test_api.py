83237a1221ca894e3a7e33d2b44cd10a7743031c
Reketino <bjornevensk8@gmail.com>
2026-03-07 16:26:50 +0100

    Feat: imported sys & pathlib & appending sys.path no need to type pythonpath for running test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1]/"src"))
+
 from api.hue_discovery import discover_bridge_ip
 from api.hue_api import HueAPI
 
