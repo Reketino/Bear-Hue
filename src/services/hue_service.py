684b7b06dc1977c5787577322fc2a9f1919b602b
Reketino <bjornevensk8@gmail.com>
2026-03-08 10:48:59 +0100

    Feat: setup Service for tunring lights on & off

---
+++ src/services/hue_service.py
@@ -0,0 +1,15 @@
+from api.hue_api import HueAPI
+
+class HueService: 
+    
+    def __init__(self, hue_api: HueAPI):
+        self.hue_api = hue_api
+        
+    def get_lights(self):
+        return self.hue_api.list_lights()
+    
+    def turn_on(self, light_id: int):
+        self.hue_api.set_light(light_id, True)
+        
+    def turn_off(self, light_id: int):
+        self.hue_api.set_light(light_id, False)
