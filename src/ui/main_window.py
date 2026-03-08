3b096ede460bf7bdd77074c5374ff5c62f8a227d
Reketino <bjornevensk8@gmail.com>
2026-03-08 11:20:34 +0100

    Feat: Set up Main window

---
+++ src/ui/main_window.py
@@ -0,0 +1,27 @@
+import customtkinter as ctk
+from services.hue_service import HueService
+
+class MainWindow(ctk.CTk):
+    
+    def __init__(self, hue_service: HueService) -> None:
+        super().__init__()
+        
+        self.hue_service = hue_service
+        
+        self.title("Bear Hue")
+        self.geometry("300x300")
+        
+        lights = hue_service.get_lights()
+        
+        for light_id, name in lights.items():
+            
+            button = ctk.CTkButton(
+                self,
+                text=name,
+                command=lambda i=light_id: self.toggle_light(i)
+            )
+            
+            button.pack(pady=10)
+            
+    def toggle_light(self, light_id: str):
+        self.hue_service.turn_on(int(light_id))
