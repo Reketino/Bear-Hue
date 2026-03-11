fcbec4da17dcba4179fd7a5fe578cd3d57d1b131
Reketino <bjornevensk8@gmail.com>
2026-03-11 19:26:28 +0100

    Refactor: more air between functions in hue_service

--- src/services/hue_service.py
+++ src/services/hue_service.py
@@ -5,23 +5,29 @@ class HueService:
     def __init__(self, hue_api: HueAPI):
         self.hue_api = hue_api
         
+        
     def get_lights(self):
         return self.hue_api.list_lights()
+    
     
     def get_light_state(self, light_id: int) -> bool:
         return self.hue_api.get_light_state(light_id)
     
+    
     def turn_on(self, light_id: int):
         self.hue_api.set_light(light_id, True)
+      
         
     def turn_off(self, light_id: int):
         self.hue_api.set_light(light_id, False)
+        
         
     def toggle(self, light_id: int):
         
         is_on = self.hue_api.get_light_state(light_id)
         
         self.hue_api.set_light(light_id, not is_on)
+    
         
     def turn_all_on(self):
         
@@ -29,6 +35,7 @@ class HueService:
         
         for light_id in lights.keys():
             self.hue_api.set_light(light_id, True)
+      
             
     def turn_off_all(self):
         
