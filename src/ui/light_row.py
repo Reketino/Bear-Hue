bef5a6e49c1459bfcc7f553ef0780592589478bc
Reketino <bjornevensk8@gmail.com>
2026-03-12 10:25:48 +0100

    Feat: setup self.hue_service, light_id & pack

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -5,3 +5,8 @@ class LightRow(ctk.CTkFrame):
     
     def __init__(self, master, hue_service: HueService, light_id: int, name: str):
         super().__init__(master)
+        
+        self.hue_service = hue_service
+        self.light_id = light_id
+        
+        self.pack(fill="x", pady=6)
