2716486b722038a3494e1607f69722517c0965e8
Reketino <bjornevensk8@gmail.com>
2026-03-12 10:24:07 +0100

    Feat: setup lightrow class + init

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -1,2 +1,7 @@
 import customtkinter as ctk
 from src.services.hue_service import HueService
+
+class LightRow(ctk.CTkFrame):
+    
+    def __init__(self, master, hue_service: HueService, light_id: int, name: str):
+        super().__init__(master)
