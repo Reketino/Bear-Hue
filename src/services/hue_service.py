711d62a3db1fdd902862b6b113a1de13ce9e93fb
Reketino <bjornevensk8@gmail.com>
2026-03-14 10:50:15 +0100

    Refactor: State collecting of lights refactored w/ cache

--- src/services/hue_service.py
+++ src/services/hue_service.py
@@ -19,14 +19,12 @@ class HueService:
     
     
     def get_all_lights_state(self):
-        lights = self.hue_api.get_all_lights_state()
+        lights = self._get_lights_cached()
         result = {}
         for light_id, data in lights.items():
-            is_on = data["state"]["on"]
-            bri = data["state"]["bri"]
             result[int(light_id)] = {
-                "on": is_on,
-                "brightness": int(bri / 2.54)
+                "on": data["state"]["on"],
+                "brightness": int(data["state"]["bri"] / 2.54)
             }
         return result
     
@@ -35,7 +33,7 @@ class HueService:
         now = time.time()
         if self._lights_cache and (now - self._cache_time) < 0.5:
             return self._lights_cache
-        lights = self.hue_api.get_all_lights_state
+        lights = self.hue_api.get_all_lights_state()
         self._lights_cache = lights
         self._cache_time = now
         return lights
