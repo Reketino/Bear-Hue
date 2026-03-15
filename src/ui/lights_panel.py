4101ee0eeaa02e68eb5e78853b70b73d2df9f95a
Reketino <bjornevensk8@gmail.com>
2026-03-15 10:25:56 +0100

    Feat: init logic hueservice & lightrows defined & set up

--- src/ui/lights_panel.py
+++ src/ui/lights_panel.py
@@ -2,3 +2,11 @@ import customtkinter as ctk
 
 from src.ui.light_row import LightRow
 from src.services.hue_service import HueService
+
+class LightsPanel(ctk.CTkScrollableFrame):
+    
+    def __init__(self, master, hue_service: HueService):
+        super().__init__(master)
+        
+        self.hue_service = hue_service 
+        self.light_rows = {}
