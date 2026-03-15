c193e2fa28a1b8ca8984a534e578429351bead11
Reketino <bjornevensk8@gmail.com>
2026-03-16 00:01:52 +0100

    Feat: setup init & pack in scenesbar

--- src/ui/scenes_bar.py
+++ src/ui/scenes_bar.py
@@ -1,3 +1,8 @@
 import customtkinter as ctk
 
 class ScenesBar(ctk.CTkFrame):
+    
+    def __init__(self, master, movie, relax, bright):
+        super().__init__(master)
+        
+        self.pack(fill="x", padx=20, pady=10)
