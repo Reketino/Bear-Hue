7f33ad36c754a2d021e08fa67cb576cc6d2ed9fc
Reketino <bjornevensk8@gmail.com>
2026-03-20 14:05:47 +0100

    Refactor: removed .keys & used lights instead

--- src/api/hue_api.py
+++ src/api/hue_api.py
@@ -60,5 +60,5 @@ class HueAPI:
         
     def set_group_brightness(self, bri: int):
         lights = self.get_all_lights_state()
-        for light_id in lights.keys():
+        for light_id in lights():
             self.set_brightness(int(light_id), bri)
