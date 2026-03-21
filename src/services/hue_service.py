e12fafcabc425a76858a96be725a56763bef067c
Reketino <bjornevensk8@gmail.com>
2026-03-21 10:11:13 +0100

    Refactor: removed cache logic from toggle

--- src/services/hue_service.py
+++ src/services/hue_service.py
@@ -55,7 +55,7 @@ class HueService:
     def toggle(self, light_id: int):
         is_on = self.get_light_state(light_id)
         self.hue_api.set_light(light_id, not is_on)
-        self._invalidate_cache()
+    
     
         
     def turn_all_on(self):
