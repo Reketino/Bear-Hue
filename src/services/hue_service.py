ad02759c3ee6b0eecc16bd1aae8d4fbd97e4286b
Reketino <bjornevensk8@gmail.com>
2026-03-21 10:11:56 +0100

    Refactor: removed air between lines

--- src/services/hue_service.py
+++ src/services/hue_service.py
@@ -55,9 +55,7 @@ class HueService:
     def toggle(self, light_id: int):
         is_on = self.get_light_state(light_id)
         self.hue_api.set_light(light_id, not is_on)
-    
-    
-        
+         
     def turn_all_on(self):
         lights = self._get_lights_cached()
         for light_id in lights.keys():
