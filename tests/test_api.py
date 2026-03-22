d90da01a8d6b980988b7673bdd7e39fa06fddaf2
Reketino <bjornevensk8@gmail.com>
2026-03-23 00:04:07 +0100

    Docs: documentation on scene test in test script

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,12 +16,17 @@ def run_test():
     
     hue = HueAPI(bridge_ip)
     
+    # TEST 1: Lights
     lights = hue.list_lights()
     
     print("\nWe Found your lights:\n")
     
     for light_id, name in lights.items():
         print(f"{light_id}: {name}")
+        
+    
+    # TEST 2: Scenes    
+    
         
 # Run script w/ python -m tests.test_api
 if __name__ == "__main__":
