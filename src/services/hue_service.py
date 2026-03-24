a922d0a8a9de5882e3fd84574c021acd72a1359a
Reketino <bjornevensk8@gmail.com>
2026-03-24 11:16:58 +0100

    Feat: setup converter for rgb to hsv using colorsys in hue to hex w/ return format

--- src/services/hue_service.py
+++ src/services/hue_service.py
@@ -17,6 +17,12 @@ class HueService:
         h = hue / 65536
         s = sat / 254
         v = bri / 254
+        r, g, b = colorsys.hsv_to_rgb(h, s, v)
+        return "#{:02x}{:02x}{:02x}".format(
+            int(r * 255),
+            int(g * 255),
+            int(b * 255),
+        )
         
     
       
