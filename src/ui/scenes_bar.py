5433368fde63a9f31bb468253c49851c7700740b
Reketino <bjornevensk8@gmail.com>
2026-03-24 10:02:01 +0100

    Chore: removed logic from ScenesBar before refactor

--- src/ui/scenes_bar.py
+++ src/ui/scenes_bar.py
@@ -7,23 +7,4 @@ class ScenesBar(ctk.CTkFrame):
         
         self.pack(fill="x", padx=20, pady=10)
         
-        movie_btn = ctk.CTkButton(
-            self,
-            text="Movie",
-            command=movie
-        )
-        movie_btn.pack(side="left", expand=True, padx=5)
         
-        relax_btn = ctk.CTkButton(
-            self,
-            text="Relax",
-            command=relax
-        )
-        relax_btn.pack(side="left", expand=True, padx=5)
-        
-        bright_btn = ctk.CTkButton(
-            self,
-            text="Bright",
-            command=bright
-        )
-        bright_btn.pack(side="left", expand=True, padx=5)
