f15f4333f7cfb326198dcf8a5bcc07bfe6628e76
Reketino <bjornevensk8@gmail.com>
2026-03-24 10:06:18 +0100

    Feat: setup scenes logic

--- src/ui/scenes_bar.py
+++ src/ui/scenes_bar.py
@@ -4,7 +4,8 @@ class ScenesBar(ctk.CTkFrame):
     
     def __init__(self, master, hue_service):
         super().__init__(master)
-        
         self.pack(fill="x", padx=20, pady=10)
-        
+        scenes = hue_service.get_scenes()
+        for scene in scenes:
+            
         
