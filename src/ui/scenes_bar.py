5469fe447fe7bee7a7f3b278a811479fbff0c55a
Reketino <bjornevensk8@gmail.com>
2026-03-25 11:54:13 +0100

    Feat: setup standard formula for luminance in _is_light

--- src/ui/scenes_bar.py
+++ src/ui/scenes_bar.py
@@ -41,4 +41,5 @@ class ScenesBar(ctk.CTkFrame):
         r = int(hex_color[0:2], 16)
         g = int(hex_color[2:4], 16)
         b = int(hex_color[4:6], 16)
+        luminance = (0.299 * r + 0.587 * g + 0.1114 * b)
         
