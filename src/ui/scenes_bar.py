9873685d5855e843bfb2cd9d52b1bf7d8aa981e8
Reketino <bjornevensk8@gmail.com>
2026-03-27 09:44:10 +0100

    Fix: indenting in try logic + Refactor: darken logic refactored w/ clamp

--- src/ui/scenes_bar.py
+++ src/ui/scenes_bar.py
@@ -62,9 +62,9 @@ class ScenesBar(ctk.CTkFrame):
         if len(hex_color) != 6:
             return "#333333"
         try:
-        r = int(hex_color[0:2], 16)
-        g = int(hex_color[2:4], 16)
-        b = int(hex_color[4:6], 16)
+            r = int(hex_color[0:2], 16)
+            g = int(hex_color[2:4], 16)
+            b = int(hex_color[4:6], 16)
         except ValueError:
             return "#333333"
         
