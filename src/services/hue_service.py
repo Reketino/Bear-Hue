74d6776618570c3f1a9de5c498cbf4dbb52ec26a
Reketino <bjornevensk8@gmail.com>
2026-03-30 12:01:45 +0200

    Chore: removed hue to hex helper function and using import from colorutils instead

--- src/services/hue_service.py
+++ src/services/hue_service.py
@@ -18,19 +18,7 @@ class HueService:
         if self.debug:
             print(*args)
    
-    def _hue_to_hex(self, hue, sat, bri):
-        import colorsys
-        h = hue / 65536
-        s = sat / 254
-        v = bri / 254
-        r, g, b = colorsys.hsv_to_rgb(h, s, v)
-        return "#{:02x}{:02x}{:02x}".format(
-            int(r * 255),
-            int(g * 255),
-            int(b * 255),
-        )
-                
-              
+                           
     # -------- READ Lights Logic --------
          
     def get_lights(self):
