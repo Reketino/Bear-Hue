e540f08ca9237cb1b4caf8619489cbad74126d1e
Reketino <bjornevensk8@gmail.com>
2026-03-30 13:05:07 +0200

    Style: button not showing scene_color anymore

--- src/ui/scenes_bar.py
+++ src/ui/scenes_bar.py
@@ -25,7 +25,7 @@ class ScenesBar(ctk.CTkFrame):
                 self,
                 text=scene["name"],
                 height=70,
-                fg_color=self._darken(color, 0.75),
+                fg_color="#2B2B2B",
                 hover_color=self._lighten(color, 1.1),
                 text_color="black" if self._is_light(color) else "white",
                 corner_radius=15,
