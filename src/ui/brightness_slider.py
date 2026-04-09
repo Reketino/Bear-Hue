668b3c54d42c591a38000a7912e22b8439dfb2eb
Reketino <bjornevensk8@gmail.com>
2026-04-09 11:42:19 +0200

    Feat: setup slight delay before processing the sliders value

--- src/ui/brightness_slider.py
+++ src/ui/brightness_slider.py
@@ -48,6 +48,7 @@ class BrightnessSlider(ctk.CTkFrame):
         slider_height = self.slider.winfo_height()
         y = slider_y + slider_height / 2
         self.bear_icon.place(x=x, y=y, anchor="center")
+        self.after(50, lambda: self._on_slide(self.slider.get()))
             
     def hide_bear(self):
         self.bear_icon.place_forget()
