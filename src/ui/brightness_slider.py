385277dd34fcd712ea01fe8cbe412c7aca16f032
Reketino <bjornevensk8@gmail.com>
2026-04-10 00:03:39 +0200

    Feat: setup slider.bind logic for start drag

--- src/ui/brightness_slider.py
+++ src/ui/brightness_slider.py
@@ -21,6 +21,7 @@ class BrightnessSlider(ctk.CTkFrame):
             command=self._on_slide
         )
         self.slider.pack(fill="x", padx=10, pady=10)
+        self.slider.bind("<ButtonPress-1>")
         
         self.bear_icon = ctk.CTkLabel(
             self,
