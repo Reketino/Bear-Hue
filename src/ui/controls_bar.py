eaafc9ef7d3fda8a3b9924fcf165b61bdccc3a50
Reketino <bjornevensk8@gmail.com>
2026-04-15 00:03:34 +0200

    Style: Fg_color on container changed

--- src/ui/controls_bar.py
+++ src/ui/controls_bar.py
@@ -5,7 +5,7 @@ class ControlsBar(ctk.CTkFrame):
     def __init__(self, master, on_all, off_all, toggle_bear_mode):
         super().__init__(master, fg_color="transparent")
         
-        container = ctk.CTkFrame(self, fg_color="#232323", corner_radius=15)
+        container = ctk.CTkFrame(self, fg_color="#1F1F1F", corner_radius=15)
         container.pack(fill="x", padx=15, pady=10)
         
         self.bear_banner = ctk.CTkLabel(
