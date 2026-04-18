fea3916c72e9c8c7d7d992945c273b3fc50f30a0
Reketino <bjornevensk8@gmail.com>
2026-04-19 00:00:36 +0200

    Style: set fg_color to init in LightRow

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -4,7 +4,10 @@ from src.services.hue_service import HueService
 class LightRow(ctk.CTkFrame):
     
     def __init__(self, master, hue_service: HueService, light_id: int, name: str):
-        super().__init__(master)
+        super().__init__(
+            master,
+            fg_color="#18201B",
+            )
         
         self.hue_service = hue_service
         self.light_id = light_id
