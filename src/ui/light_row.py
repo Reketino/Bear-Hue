4c253a8fc734d8ff45ae40e0a4583180f0033749
Reketino <bjornevensk8@gmail.com>
2026-04-20 00:15:54 +0200

    Style: w/ corner radius

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -25,7 +25,8 @@ class LightRow(ctk.CTkFrame):
             
             fg_color="transparent",
             hover_color="#22382D",
-            text_color="#E6F2EC"
+            text_color="#E6F2EC",
+            corner_radius=10
         )
         button.pack(side="left", fill="x", expand=True, padx= 10)
         
