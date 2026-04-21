5e91a286056d22bb08a69c8f687946b83bc3490e
Reketino <bjornevensk8@gmail.com>
2026-04-22 00:06:14 +0200

    Feat: Added self.bind effect in init to create hover effect

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -59,6 +59,7 @@ class LightRow(ctk.CTkFrame):
         )
         self.brightness_label.pack(side="right", padx=10)
         self.refresh_status()
+        self.bind("<Enter>", lambda e: self.configure(fg_color="#1E2A22"))
      
         
     def toggle_light(self):
