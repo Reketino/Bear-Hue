efdfebe7b431de380da797ae5762a532aeac0e8a
Reketino <bjornevensk8@gmail.com>
2026-05-13 10:50:06 +0200

    chore: removed updated bear position from slider & related logic to bear icon

--- src/ui/brightness_slider.py
+++ src/ui/brightness_slider.py
@@ -32,35 +32,9 @@ class BrightnessSlider(ctk.CTkFrame):
         self.after(100, self.update_bear_position)
         
     def _on_slide(self, value):
-        self.update_bear_position()
         if self.external_command:
             self.external_command(value)
                    
-    def update_bear_position(self):
-        self.update_idletasks()
-        val = self.slider.get()
-        width = self.slider.winfo_width()
-        if width <= 1:
-            return
-        knob_size = 10
-        track_padding = 0
-        usable = width - (track_padding * 2)
-        x = track_padding + (val / 100) * usable
-        x = x - knob_size / 2
-        self.bear_icon.place(
-            in_=self.slider,
-            x=x,
-            rely=0.5,
-            anchor="center"
-        )
-          
-    def show_bear(self):
-        self.update_bear_position()
-        self.bear_icon.lift()
-            
-    def hide_bear(self):
-        self.bear_icon.place_forget()
-        
     def _start_drag(self, _):
         self.is_dragging = True
         
