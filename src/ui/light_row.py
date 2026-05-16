dfd1a93d25c2a737507889fd23bfa5b872f76ee8
Reketino <bjornevensk8@gmail.com>
2026-05-16 10:13:28 +0200

    feat: setup staus color w/ configurment of status chore: removed old logic in update_state

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -108,10 +108,9 @@ class LightRow(ctk.CTkFrame):
         
         
     def update_state(self, is_on: bool, brightness: int):
-        color = "green" if is_on else "red"
-        self.status.configure(text_color=color)
-        self.brightness_label.configure(text=f"{brightness}%")
-        self.brightness_bar.set(brightness / 100)
-        
+        status_color = "#6BAF92" if is_on else "#A94442"
+        self.status.configure(
+            text_color=status_color
+        )
         
         
