e70d6a7896152a5e16e940799c4d47599cfb1d33
Reketino <bjornevensk8@gmail.com>
2026-05-17 13:15:13 +0200

    fix: Managed to add update state inside refresh_status, forgetting the indenting is actually something I prefer

--- src/ui/light_row.py
+++ src/ui/light_row.py
@@ -111,21 +111,21 @@ class LightRow(ctk.CTkFrame):
             self.refresh_status
         )
         
-        def update_state(self, is_on: bool, brightness: int):
-            status_color = (
-                "#6BAF92"
-                if is_on 
-                else "#A94442"
-            )
-            self.status.configure(
-                text_color=status_color
-            )
-            self.brightness_label.configure(
-                text=f"{brightness:>3}%"
-            )
-            self.brightness_bar.set(
-                brightness / 100
-            ) 
+    def update_state(self, is_on: bool, brightness: int):
+        status_color = (
+            "#6BAF92"
+            if is_on 
+            else "#A94442"
+        )
+        self.status.configure(
+            text_color=status_color
+        )
+        self.brightness_label.configure(
+            text=f"{brightness:>3}%"
+        )
+        self.brightness_bar.set(
+            brightness / 100
+        ) 
             
         
         
