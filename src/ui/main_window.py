8bfc9f18710d56fd4e79bd9dd528a5534fab4d81
Reketino <bjornevensk8@gmail.com>
2026-05-19 10:58:32 +0200

    feat: added slider config in default theme w/ progress_color

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -72,6 +72,11 @@ class MainWindow(ctk.CTk):
     def default_theme(self):
         self.configure(fg_color="#101010")
         self.controls.glow.configure(fg_color="transparent")
+        self.brightness.slider.configure(
+            progress_color=self._default_slider_style[
+                "progress_color"
+            ],
+        )
             
     def enable_bear_mode(self):
         self.configure(fg_color="#0B0F0D")
