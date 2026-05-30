014723ed1d354857bac69dc23066ec4574553e4c
Reketino <bjornevensk8@gmail.com>
2026-05-31 00:03:22 +0200

    style: changed height on brightness slider to 20

--- src/ui/brightness_slider.py
+++ src/ui/brightness_slider.py
@@ -17,7 +17,7 @@ class BrightnessSlider(ctk.CTkFrame):
             to=100,
             number_of_steps=100,
             command=self._on_slide,
-            height=28
+            height=20
         )
         self.slider.pack(fill="x", padx=30, pady=20)
         self.slider.bind("<ButtonPress-1>", self._start_drag)
