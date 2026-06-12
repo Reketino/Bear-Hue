2e12ea6fd48c4456a591ea9e510ef36087354774
Reketino <bjornevensk8@gmail.com>
2026-06-13 00:02:26 +0200

    feat: added def init to scene card

--- src/ui/scene_card.py
+++ src/ui/scene_card.py
@@ -1,3 +1,11 @@
 import customtkinter as ctk
 
 class SceneCard(ctk.CTkFrame):
+    
+    def __init__(
+        self,
+        master,
+        scene: dict,
+        palette: list[str],
+        command
+    ):
