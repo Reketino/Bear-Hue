aa9358dd82ae7c0e96dc74f83c27c54122ea5138
Reketino <bjornevensk8@gmail.com>
2026-08-03 10:13:25 +0200

    feat: added class w/ def init logic in settings_window.py

--- src/ui/settings_window.py
+++ src/ui/settings_window.py
@@ -1,3 +1,12 @@
 import customtkinter as ctk
 
 from src.settings.settings_manager import SettingsManager
+
+
+class SettingsWindow(ctk.CTkToplevel):
+    def __init__(
+        self,
+        master,
+        settings: SettingsManager,
+    ) -> None:
+        super().__init__(master)
